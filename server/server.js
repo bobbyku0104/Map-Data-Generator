1c1a4a14ad4d709d02fce1660d3c901f2969d501
Prakash Kumar <prakashyadav9576@gmail.com>
2026-04-17 23:13:21 +0530

    get client details

--- server/server.js
+++ server/server.js
@@ -41,6 +41,34 @@ app.post("/leads", async (req, res) => {
   }
 });
 
+app.get("/clients/:keyword", async (req, res) => {
+  const api_key = process.env.OPENWEBNINJA_API_KEY;
+  try {
+    const keyword = req.params.keyword;
+
+    const response = await axios.get(
+      "https://api.openwebninja.com/local-business-data/search",
+      {
+        headers: {
+          "X-API-Key": api_key,
+          Accept: "*/*",
+        },
+        params: {
+          query: keyword,
+          limit: 20,
+        },
+      },
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error:", error.response?.data || error.message);
+    res.status(500).json({
+      error: "Failed to fetch data",
+    });
+  }
+});
+
 // Server start (ONLY ONCE)
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
