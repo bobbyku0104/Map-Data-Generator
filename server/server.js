fb3f96df8650b44282a1727c5f5bd7e519ad72d6
bobbyku0104 <143bobby123@gmail.com>
2026-06-12 23:26:32 +0530

    remove error

--- server/server.js
+++ server/server.js
@@ -17,6 +17,9 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "http://localhost:5174",
+  "http://127.0.0.1:5174",
   "https://map-data-generator.vercel.app"
 ];
 
